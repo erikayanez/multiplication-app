ea3e60f70888d8ff06f203bd8269f0be8e1cfcd6
“erikayanez“ <erikajoam@gmail.com>
2023-03-08 14:05:20 +0100

    Errors were fixed.  Score changes depending on the answer given.  The app works as spected

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const num1 = Math.ceil(Math.random()*10);
+console.log(num1);
+
 const num2 = Math.ceil(Math.random()*10);
+console.log(num2);
 
 const questionEl = document.getElementById("question");
-
 const formEl = document.getElementById("form");
 const inputEl = document.getElementById("input");
 
-let score = 0;
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`
 
 questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
 
-const correctAnswer = num1 * num2;
+const correctAns = num1 * num2;
 
 formEl.addEventListener("submit", ()=> {
-    const userAnswer = +inputEl.value;
-    if(userAnswer === correctAnswer){
-        score++;
-        console.log(score)
+    const userAns = +inputEl.value;
+    if(userAns === correctAns){
+        score++
+        updateLocalStorage();
     }else{
-        score--;
-        console.log(score)
+        score--
+        updateLocalStorage();
     }
-    });
+});
 
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score));
+}
